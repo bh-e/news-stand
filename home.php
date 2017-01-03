d3947030ddf1efdeca88ede55aef05263cca86cd
Abhijith PA <abhijith@openmailbox.org>
2017-01-03 15:04:02 +0530

    removed things

--- home.php
+++ home.php
@@ -21,27 +21,6 @@
             }								
         });
     </script>
- <?php          
-    $sql = "SELECT * FROM user where user_username='$user_username'";
-    $result = mysqli_query($database,$sql) or die(mysqli_error($database)); 
-    $rws = mysqli_fetch_array($result);
-?>   
-<div class="container">
-    <div class="row clearfix">
-        <div class="col-md-12">
-            <h1 class="text-center">Welcome to your profile</h1>
-        </div>
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-            <ul class="nav text-center">
-                <li><a href="edit-profile.php">Edit your profile</a></li>
-                <li><a href="all-users.php">View all users</a></li>
-                <li><a href="components/logout.php">Logout</a></li>
-	        <li><a href="profile.php?user_username=<?php echo $rws['user_username'];?>">See your profile</a></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div>
     <?php
 
     	$rss = new DOMDocument();
